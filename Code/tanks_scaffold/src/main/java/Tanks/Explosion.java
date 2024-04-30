108e4ec8dc9331e88f2d9e59abc25a0eaffb9aaf
Henry Nguyen <nguyenchihieu178205@gmail.com>
2024-04-30 20:48:44 +1000

    update jdocs explosion

--- Code/tanks_scaffold/src/main/java/Tanks/Explosion.java
+++ Code/tanks_scaffold/src/main/java/Tanks/Explosion.java
@@ -3,47 +3,52 @@ package Tanks;
 import processing.core.PApplet;
 
 public class Explosion {
-    private final PApplet app;
+    private final PApplet APP;
+    private final int RADIUS;
     private final float xPos;
     private final float yPos;
     private float startTime = 0;
-    private final int radius;
     private boolean finishedExplode = false;
 
     public Explosion (PApplet app, float xPos, float yPos, int radius){
-        this.app = app;
+        this.APP = app;
         this.xPos = xPos;
         this.yPos = yPos;
-        this.radius = radius;
+        this.RADIUS = radius;
     }
 
+    /**
+     * Draw the explosion with specified radius.
+     */
     public void drawExplosion(){
+        // Set start time
         if (startTime == 0){
-            startTime = app.millis();
+            startTime = APP.millis();
         }
         float animationDuration = 200f;
-        float elapsedTime = app.millis()-startTime;
+        float elapsedTime = APP.millis()-startTime;
 
-        float currentRedRadius = Math.min(radius * elapsedTime / animationDuration, radius);
-        float currentOrangeRadius = Math.min(radius * 0.5f * elapsedTime / animationDuration, radius * 0.5f);
-        float currentYellowRadius = Math.min(radius * 0.2f * elapsedTime / animationDuration, radius * 0.2f);
+        float currentRedRadius = Math.min(RADIUS * elapsedTime / animationDuration, RADIUS);
+        float currentOrangeRadius = Math.min(RADIUS * 0.5f * elapsedTime / animationDuration, RADIUS * 0.5f);
+        float currentYellowRadius = Math.min(RADIUS * 0.2f * elapsedTime / animationDuration, RADIUS * 0.2f);
 
-        int[] colors = {app.color(255, 0, 0), app.color(255, 165, 0), app.color(255, 255, 0)};
+        int[] colors = {APP.color(255, 0, 0), APP.color(255, 165, 0), APP.color(255, 255, 0)};
+        // If explosion haven't finished, keep drawing
         if (!finishedExplode) {
-            app.noStroke();
+            APP.noStroke();
 
             if (elapsedTime <= animationDuration) {
-                app.fill(colors[0]);
-                app.ellipse(xPos, yPos, currentRedRadius * 2, currentRedRadius * 2);
-                app.fill(colors[1]);
-                app.ellipse(xPos, yPos, currentOrangeRadius * 2, currentOrangeRadius * 2);
-                app.fill(colors[2]);
-                app.ellipse(xPos, yPos, currentYellowRadius * 2, currentYellowRadius * 2);
+                APP.fill(colors[0]);
+                APP.ellipse(xPos, yPos, currentRedRadius * 2, currentRedRadius * 2);
+                APP.fill(colors[1]);
+                APP.ellipse(xPos, yPos, currentOrangeRadius * 2, currentOrangeRadius * 2);
+                APP.fill(colors[2]);
+                APP.ellipse(xPos, yPos, currentYellowRadius * 2, currentYellowRadius * 2);
 
             } else finishedExplode = true;
 
             //reset
-            app.stroke(0);
+            APP.stroke(0);
         }
     }
 
