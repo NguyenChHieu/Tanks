7ce3e4948d6a5c3fff043a0966d7bb92760946ab
Henry Nguyen <nguyenchihieu178205@gmail.com>
2024-05-07 22:53:04 +1000

    add tank explosion

--- Code/tanks_scaffold/src/test/java/Tanks/ExplosionTest.java
+++ Code/tanks_scaffold/src/test/java/Tanks/ExplosionTest.java
@@ -31,4 +31,39 @@ public class ExplosionTest {
 //
 //        assertEquals(1, app.getTanksAlive().size(), "Incorrect number of alive tanks.");
 //    }
+
+
+    /**
+     * Tests the tank's explosion radius (15)
+     * when the tank's health is 0.
+     */
+    @Test
+    public void testTankExplosion(){
+        App app = new App();
+        app.loop();
+        app.setConfigPath("additionalFiles/testMap.json");
+        PApplet.runSketch(new String[]{"App"}, app);
+        // Setup delay
+        app.delay(1000);
+
+        get.printPrompt("testTankExplosion", false);
+
+        // Set health of the tank to low so that we can test the explosion quicker
+        app.getTanksAlive().get(0).tankLoseHP(90);
+
+        // Decrease power to 0
+        for (int i = 0; i < 50; i++) {
+            app.keyPressed(new KeyEvent(null, 0, 0, 0, 'S', 83));
+        }
+
+        // Shoots
+        app.keyPressed(new KeyEvent(null, 0, 0, 0, ' ', 32));
+        app.delay(2000);
+
+        // Test
+        Tank testTank = app.getTanksAlive().get(0);
+        assertEquals("B", testTank.type, "The current tank should be B after A shoots.");
+        assertEquals(1, app.getTanksAlive().size(), "There should be only 1 tank left.");
+        System.out.println("testTankExplosion passed");
+    }
 }
